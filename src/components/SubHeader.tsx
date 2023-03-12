db94fa3d01493189b1aaef58ca41255f57894408
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-03-11 22:50:09 -0300

    feat: Created and styled component SubHeader

---
+++ src/components/SubHeader.tsx
@@ -0,0 +1,11 @@
+const SubHeader = () => {
+  return (
+    <div className="w-full h-16 bg-custom-gray-8 flex flex-row justify-center items-center">
+      <div className="w-full max-w-603">
+        <p className="text-custom-gray-1 font-medium text-xl">Feed</p>
+      </div>
+    </div>
+  );
+};
+
+export { SubHeader };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { Header } from "../components/Header";
+import { SubHeader } from "../components/SubHeader";
 import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
 
@@ -8,7 +9,7 @@ const AllRoutes = () => {
     <Routes>
       <Route path="/" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
-      <Route path="/dashboard" element={<Header />} />
+      <Route path="/dashboard" element={<SubHeader />} />
     </Routes>
   );
 };
